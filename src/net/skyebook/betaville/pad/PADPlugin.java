253a70a703cb05dcb0f930c9042c4077a9fede4e
Skye Book <skye.book@gmail.com>
2011-10-06 01:26:45 -0400

    PAD plugin now parses the downloaded files

--- src/net/skyebook/betaville/pad/PADPlugin.java
+++ src/net/skyebook/betaville/pad/PADPlugin.java
@@ -10,6 +10,12 @@ import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.List;
+
+import net.skyebook.padloader.database.DerbyImplementation;
+import net.skyebook.padloader.read.ADRReader;
+import net.skyebook.padloader.read.BBLReader;
+import net.skyebook.padloader.record.Record;
 
 import org.fenggui.FengGUI;
 
@@ -46,14 +52,21 @@ public class PADPlugin extends Plugin{
 		System.out.println("loading window created");
 
 		FengUtils.putAtMiddle(loadingWindow, GUIGameState.getInstance().getDisp());
-		
+
 		System.out.println("loading window placed in scene");
 
 		SettingsPreferences.getThreadPool().execute(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				// Check if the data has already been loaded into Derby
+				DerbyImplementation derby = new DerbyImplementation(new File(getDataDirectory().toString()+"/db/").toString());
+				if(derby.tablesWereAlreadyCreated()){
+					System.out.println("Using pre-existing tables");
+				}
+				else{
+					System.out.println("Fresh tables were created");
+				}
 
 				// If it hasn't, check if the PAD archive has already been downloaded
 				if(!doesPADDataExist()){
@@ -64,14 +77,71 @@ public class PADPlugin extends Plugin{
 						loadingWindow.setDLStatus("Could not download data file");
 					}
 				}
+
+				// now we can load the data
+				try {
+					File padDirectory = new File(getDataDirectory().toString()+"/"+PAD_FILENAME.substring(0, PAD_FILENAME.indexOf("."))+"/");
+
+					File addrFile = new File(padDirectory.toString()+"/bobaadr.txt");
+					loadingWindow.setADRStatus("Parsing Raw Data");
+					List<Record> adr = new ADRReader().readRecords(addrFile);
+
+					System.out.println(adr.size() + " records read");
+
+					long dbStart = System.currentTimeMillis();
+
+					int updateInterval=1000;
+					
+					loadingWindow.setADRStatus("Inserting Records");
+					
+					int counter = 0;
+					for(Record record : adr){
+						counter++;
+						if(counter%updateInterval==0) loadingWindow.setADRProgress(counter/1000, adr.size()/1000);
+						derby.addRecord(record);
+					}
+
+					System.out.println("ADR database insert took " + (System.currentTimeMillis()-dbStart)+"ms");
+
+					// release the resources
+					adr = null;
+					
+					loadingWindow.setADRStatus("Complete");
+
+
+
+					File bblFile = new File(padDirectory.toString()+"/bobabbl.txt");
+					loadingWindow.setBBLStatus("Parsing Raw Data");
+					List<Record> bbl = new BBLReader().readRecords(bblFile);
+					System.out.println(bbl.size() + " records read");
+
+					dbStart = System.currentTimeMillis();
+
+					loadingWindow.setBBLStatus("Inserting Records");
+					
+					counter = 0;
+					for(Record record : bbl){
+						counter++;
+						if(counter%updateInterval==0) loadingWindow.setBBLProgress(counter/1000, bbl.size()/1000);
+						derby.addRecord(record);
+					}
+
+					System.out.println("BBL database insert took " + (System.currentTimeMillis()-dbStart)+"ms");
+
+					// release the resources
+					bbl = null;
+					
+					loadingWindow.setBBLStatus("Complete");
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		});
 	}
-
+	
 	@Override
 	public void destroy() {
-		// TODO Auto-generated method stub
-
 	}
 
 	private boolean doesPADDataExist(){
@@ -106,12 +176,12 @@ public class PADPlugin extends Plugin{
 				outputStream.write(byteValue);
 			}
 			outputStream.close();
-			
+
 			loadingWindow.setDLStatus("Extracting Files");
-			
+
 			// unzip the files
 			Unzipper.unzip(padArchive, null);
-			
+
 			loadingWindow.setDLStatus("Done!");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
